4c299e85aac0f9530864f59305d35364119934c9
MusharafShaik970 <musharrafshaik970@gmail.com>
2024-04-08 03:17:27 +0530

    dynamic data with API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,41 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import RestaurantCard from "./RestaurantCard"
 import resList from "../utils/mockResData"
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList)
+  const [listOfRestaurants, setListOfRestaurants] = useState([])
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    try {
+      const data = await fetch(
+        "https://www.swiggy.com/mapi/homepage/getCards?lat=17.37240&lng=78.43780"
+      )
+      const response = await data.json()
+      console.log(response)
+      console.log(
+        response?.data?.success?.cards[1]?.gridWidget?.gridElements
+          ?.infoWithStyle?.restaurants
+      )
+      setListOfRestaurants(
+        response.data.success.cards[1].gridWidget.gridElements.infoWithStyle
+          .restaurants
+      )
+    } catch {
+      setTimeout(() => {
+        setListOfRestaurants(resList)
+      }, 100)
+      console.log("swiggy api is down")
+    }
+  }
+
+  if (listOfRestaurants.length === 0) {
+    return <h1>Loading .......</h1>
+  }
+
   return (
     <div className="body">
       <div className="search">
